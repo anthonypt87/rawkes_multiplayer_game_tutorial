6f2dae334c6a0bd620a853d5b466073c45bcb4f8
anthony <anthonypt87@gmail.com>
2013-01-30 23:07:38 -0800

    Starting on the game server

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-// Placeholder file for Node.js game server
+var util = require('util'),
+		io = require('socket.io');
+
+var socket,
+		players;
+
+function init() {
+	players = [];
+	socket = io.listen(8000);
+	socket.configure(function() {
+		socket.set('transports', ['websocket']);
+		socket.set('log level', 2);
+	});
+}
+
+init();
